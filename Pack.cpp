04701e725a0895829e70ab140901e21af6286bc8
DylanJaveri <140866137+DylanJaveri@users.noreply.github.com>
2025-02-18 17:09:09 -0500

    Create Pack.cpp errors included whoops

---
+++ Pack.cpp
@@ -0,0 +1,35 @@
+#include <cassert>
+#include <iostream>
+#include <array>
+#include <string>
+#include "Pack.hpp"
+
+Pack::Pack() {
+    int index = 0;
+    for (int s = SPADES; s <= DIAMONDS; ++s) {
+        Suit suit = static_cast<Suit>(s);
+        for (int r = 7; r < 13; ++r) {
+            Rank rank = static_cast<Rank>(r);
+            cards[index] = Card(rank, suit);
+            index++;
+        }
+    }
+}
+
+  Pack::Pack(std::istream& pack_input) {
+    int index = 0;
+    std::string line;
+    while (getline(pack_input, line)) {
+        std::istringstream iss(line); //error here
+        Card c; 
+        iss >> c; //error here
+        cards[index] = c;
+        index ++ 
+    }
+  }
+
+bool Pack::empty() const {
+    if (cards == (std::array<Card, 0> cards)) { //error here
+        return true;
+    };
+}
